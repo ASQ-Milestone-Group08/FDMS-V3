881f6e644edaea0197c790f2a78e7c4e872ab0d3
chris_park1004 <honggyupark1004@gmail.com>
2025-11-25 17:07:27 -0500

    Push initial projects for the Window Application development

---
+++ Aircraft-Transmission-System/Form1.cs
@@ -0,0 +1,10 @@
+namespace Aircraft_Transmission_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ground-Station-Terminal/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ground_Station_Terminal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
