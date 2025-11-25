068a4267d38a7b34305babf44f6f033bdff7920d
chris_park1004 <honggyupark1004@gmail.com>
2025-11-25 17:40:23 -0500

    Recreate all test projects and create the integrated solution file at the root dir

---
+++ src/Ground-Terminal-System/Form1.cs
@@ -0,0 +1,10 @@
+namespace GroundTerminalSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ tests/Aircraft-Transmission-System-Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AircraftTransmissionSystem.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
