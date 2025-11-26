5554b57273ab5a8272bf391b62eefa7cc52de25c
chris-park-1004 <honggyupark1004@gmail.com>
2025-11-26 13:31:13 -0500

    Implement FileTelemetryReader class
    - The constructor takes the file path of the telemetry data
    - GetNextReading() reads text data line  by line
    - Reset() closes the opened file and reopens it
    - Dispose() closes file stream and initializes StreamReader
    - Dispose() will be called by the destructor

--- src/Aircraft-Transmission-System/ITelemetrySource.cs
+++ src/Aircraft-Transmission-System/ITelemetrySource.cs
@@ -7,17 +7,27 @@ namespace AircraftTransmissionSystem
     public interface ITelemetrySource : IDisposable
     {
         /// <summary>
-        /// Retrieves the next line of telemetry data.
+        /// Function Name: GetNextReading
+        /// Description: Retrieves the next line of telemetry data from the source.
+        ///              Each call returns one line containing comma-delimited telemetry values.
+        ///              This method advances the internal reading position to the next line.
+        /// Parameters: None
+        /// Return Type: string? (nullable string)
+        ///              - Returns a string in format: Timestamp, Accel-X, Accel-Y, Accel-Z, Weight, Altitude, Pitch, Bank
+        ///              - Returns null when end of data is reached or no more data is available
         /// </summary>
         /// <returns>
-        /// A string containing telemetry data in the format:
-        /// Timestamp, Accel-X, Accel-Y, Accel-Z, Weight, Altitude, Pitch, Bank
-        /// Returns null when no more data is available.
+        /// A comma-delimited string containing 8 telemetry fields, or null if no more data is available.
         /// </returns>
         public string? GetNextReading();
 
         /// <summary>
-        /// Gets a value indicating whether more telemetry data is available.
+        /// Property Name: HasMoreData
+        /// Description: Indicates whether more telemetry data is available to read.
+        ///              This property should be checked before calling GetNextReading() to avoid null returns.
+        /// Return Type: bool
+        ///              - true: More data is available
+        ///              - false: End of data reached or source is closed
         /// </summary>
         public bool HasMoreData { get; }
     }
