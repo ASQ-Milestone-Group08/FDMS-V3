455c3caa05ee7fe42155f741c25d35a532928c9a
garciaerin <garciaerinjade@gmail.com>
2025-11-26 23:23:16 -0500

    fix: assembly references in Forms1.cs and Programs.cs. Reverted some changes.

--- src/Ground-Terminal-System/Form1.cs
+++ src/Ground-Terminal-System/Form1.cs
@@ -1,4 +1,3 @@
-using System.Windows.Forms.DataVisualization.Charting;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,15 +10,24 @@ using System.Windows.Forms;
 using Microsoft.Data.Sqlite;
 using System.Linq;
 using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
 
 namespace GroundTerminalSystem
 {
     public partial class Form1 : Form
     {
+
+        private CancellationTokenSource simSource;
+        private readonly string dbPath = "FDMS_DB";
+
         public Form1()
         {
             InitializeComponent();
+            InitializeDatabase();
+            UpdateRealTimeStatus();
         }
 
-    }
-}
+
+    }//END class
+}//END namespace
--- src/Ground-Terminal-System/Program.cs
+++ src/Ground-Terminal-System/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace GroundTerminalSystem
 {
     internal static class Program
