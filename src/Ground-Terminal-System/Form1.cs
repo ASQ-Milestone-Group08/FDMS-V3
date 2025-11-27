b2b3a4b51923f7b0bb79af3318cdd44ec2433cb9
garciaerin <garciaerinjade@gmail.com>
2025-11-27 00:46:56 -0500

    add: added the StartSim() and ComputeChecksum() for the real-time; added a Telemetry class.

--- src/Ground-Terminal-System/Form1.cs
+++ src/Ground-Terminal-System/Form1.cs
@@ -15,6 +15,22 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace GroundTerminalSystem
 {
+    //TELEMETRY CLASS
+    public class Telemetry
+    {
+        public string Tail { get; set; }
+        public DateTime Time { get; set; }
+        public double AccX { get; set; }
+        public double AccY { get; set; }
+        public double AccZ { get; set; }
+        public double Weight { get; set; }
+        public double Altitude { get; set; }
+        public double Pitch { get; set; }
+        public double Bank { get; set; }
+    }//end Telemetry class
+
+
+
     public partial class Form1 : Form
     {
 
@@ -54,15 +70,53 @@ namespace GroundTerminalSystem
 
             btnStart.Enabled = false;
             btnStop.Enabled = true;
-        }
+        }//end btnStart_Click
 
         private void btnStop_Click(object sender, EventArgs e)
         {
             simSource?.Cancel();
             btnStart.Enabled = true;
             btnStop.Enabled = false;
-        }
+        }//end btnStop_Click
 
+        private async void StartSim(CancellationToken token)
+        {
+            Random rnd = new Random();
+            double alt = 5000;
+
+            while (!token.IsCancellationRequested)
+            {
+                var pkt = new Telemetry
+                {
+                    Tail = "AC123",
+                    Time = DateTime.Now,
+                    AccX = rnd.NextDouble(),
+                    AccY = rnd.NextDouble(),
+                    AccZ = rnd.NextDouble(),
+                    Weight = 2150 + rnd.NextDouble(),
+                    Altitude = alt,
+                    Pitch = Math.Round((rnd.NextDouble() - 0.5) * 0.05, 6),
+                    Bank = Math.Round((rnd.NextDouble() - 0.5) * 0.02, 6)
+                };
+
+                alt += rnd.Next(200, 600);
+
+                int checksum = ComputeChecksum(pkt);
+
+                // Process packet
+                ProcessTelemetry(pkt, checksum);
+
+                await Task.Delay(1000);
+            }
+
+        }//end StartSim
+
+        private int ComputeChecksum(Telemetry t)
+        {
+            return (int)((t.Altitude + t.Pitch + t.Bank) / 3.0);
+        }//end ComputeChecksum
 
     }//END class
+
+    
 }//END namespace
