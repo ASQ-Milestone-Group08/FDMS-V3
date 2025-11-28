d131c23e939563bd31fe98d5083f262329302ab0
Andrei <habocandrei0627@gmail.com>
2025-11-28 02:39:24 -0500

    Updated TelemetryData class to match to the database value

--- src/Ground-Terminal-System/TelemetryData.cs
+++ src/Ground-Terminal-System/TelemetryData.cs
@@ -2,14 +2,21 @@ namespace GroundTerminalSystem
 {
     public class TelemetryData
     {
-        public string Timestamp { get; set; }
+        public required string TailNumber { get; set; }
+        public int Sequence { get; set; }
+
+        public string? TimeOfRecording { get; set; }
+        public DateTime TimeReceived { get; set; }
+
         public double AccelX { get; set; }
         public double AccelY { get; set; }
         public double AccelZ { get; set; }
         public double Weight { get; set; }
+
         public double Altitude { get; set; }
         public double Pitch { get; set; }
         public double Bank { get; set; }
+
         public int Checksum { get; set; }
     }
 }
