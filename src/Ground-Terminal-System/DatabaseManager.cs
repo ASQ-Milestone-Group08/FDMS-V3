9fa180163927177d10c824ab09a4bd394efbf33d
Andrei <habocandrei0627@gmail.com>
2025-11-28 02:37:57 -0500

    Added the DatabaseManager class with three functions. StoreAltitudeData, StoreGForceData, StoreInvalidPacket

--- src/Ground-Terminal-System/DatabaseManager.cs
+++ src/Ground-Terminal-System/DatabaseManager.cs
@@ -1,10 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
+using System.Data.SqlClient;
 
 namespace GroundTerminalSystem
 {
-    internal class DatabaseManager
+    public class DatabaseManager
     {
+        private readonly string _connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Store valid Attitude (Altitude / Pitch / Bank) values
+        public void StoreAltitudeData(TelemetryData data)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            string query = @"
+                INSERT INTO AltitudeData
+                (AircraftTailNumber, TimeOfRecording, TimeReceived, Altitude, Pitch, Bank)
+                VALUES (@TailNumber, @TimeOfRecording, @TimeReceived, @Altitude, @Pitch, @Bank);";
+
+            using SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@TailNumber", data.TailNumber);
+            cmd.Parameters.AddWithValue("@TimeOfRecording", data.TimeOfRecording);
+            cmd.Parameters.AddWithValue("@TimeReceived", data.TimeReceived);
+            cmd.Parameters.AddWithValue("@Altitude", data.Altitude);
+            cmd.Parameters.AddWithValue("@Pitch", data.Pitch);
+            cmd.Parameters.AddWithValue("@Bank", data.Bank);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        // Store valid G-Force data
+        public void StoreGForceData(TelemetryData data)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            string query = @"
+                INSERT INTO GForceData
+                (AircraftTailNumber, TimeOfRecording, TimeReceived, AccelerationX, AccelerationY, AccelerationZ, Weight)
+                VALUES (@TailNumber, @TimeOfRecording, @TimeReceived, @AccelX, @AccelY, @AccelZ, @Weight);";
+
+            using SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@TailNumber", data.TailNumber);
+            cmd.Parameters.AddWithValue("@TimeOfRecording", data.TimeOfRecording);
+            cmd.Parameters.AddWithValue("@TimeReceived", data.TimeReceived);
+            cmd.Parameters.AddWithValue("@AccelX", data.AccelX);
+            cmd.Parameters.AddWithValue("@AccelY", data.AccelY);
+            cmd.Parameters.AddWithValue("@AccelZ", data.AccelZ);
+            cmd.Parameters.AddWithValue("@Weight", data.Weight);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        // Store invalid raw packet (no parsing)
+        public void StoreInvalidPacket(string rawPacket, string tailNumber)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            string query = @"
+                INSERT INTO PacketErrorData
+                (AircraftTailNumber, TimeReceived, PacketData)
+                VALUES (@TailNumber, @TimeReceived, @PacketData);";
+
+            using SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@TailNumber", tailNumber);
+            cmd.Parameters.AddWithValue("@TimeReceived", DateTime.Now);
+            cmd.Parameters.AddWithValue("@PacketData", rawPacket);
+
+            cmd.ExecuteNonQuery();
+        }
     }
 }
